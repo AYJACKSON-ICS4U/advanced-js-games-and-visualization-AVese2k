8cef4b1c7f7eca6b8e16760d79ce7d3c32419ea4
AVese2k <31697731+AVese2k@users.noreply.github.com>
2018-01-25 21:46:25 -0500

    Create memory++.js

---
+++ memory++.js
@@ -0,0 +1,143 @@
+//added 5 more images that need to be matched
+//adjusted display of cards and mouse function
+//showed the correct and incorrect number of matches
+//formatted the results
+var Tile = function(x, y, face) {
+    this.x = x;
+    this.y = y;
+    this.face = face;
+    this.width = 70;
+};
+
+Tile.prototype.drawFaceDown = function() {
+    fill(214, 247, 202);
+    strokeWeight(2);
+    rect(this.x -15, this.y -15, this.width -15, this.width -15, 10);
+    image(getImage("avatars/leaf-green"), this.x -15, this.y -15, this.width -15, this.width -15);
+    this.isFaceUp = false;
+};
+
+Tile.prototype.drawFaceUp = function() {
+    fill(214, 247, 202);
+    strokeWeight(2);
+    rect(this.x -15, this.y -15, this.width -15, this.width -15, 10);
+    image(this.face, this.x -15, this.y -15, this.width -15, this.width -15);
+    this.isFaceUp = true;
+};
+
+Tile.prototype.isUnderMouse = function(x, y) {
+    return x >= this.x -15 && x <= this.x -15 + this.width -15  &&
+        y >= this.y -15  && y <= this.y -15 + this.width -15;
+};
+
+// Global config
+var NUM_COLS = 6;
+var NUM_ROWS = 5;
+
+// Declare an array of all possible faces
+var faces = [
+    getImage("avatars/leafers-seed"),
+    getImage("avatars/leafers-seedling"),
+    getImage("avatars/leafers-sapling"),
+    getImage("avatars/leafers-tree"),
+    getImage("avatars/leafers-ultimate"),
+    getImage("avatars/marcimus"),
+    getImage("avatars/mr-pants"),
+    getImage("avatars/mr-pink"),
+    getImage("avatars/old-spice-man"),
+    getImage("avatars/robot_female_1"),
+    getImage("avatars/aqualine-seed"),
+    getImage("avatars/aqualine-seedling"),
+    getImage("avatars/aqualine-sapling"),
+    getImage("avatars/aqualine-tree"),
+    getImage("avatars/aqualine-ultimate")
+];
+
+// Make an array which has 2 of each, then randomize it
+var possibleFaces = faces.slice(0);
+var selected = [];
+for (var i = 0; i < (NUM_COLS * NUM_ROWS) / 2; i++) {
+    // Randomly pick one from the array of remaining faces
+    var randomInd = floor(random(possibleFaces.length));
+    var face = possibleFaces[randomInd];
+    // Push twice onto array
+    selected.push(face);
+    selected.push(face);
+    // Remove from array
+    possibleFaces.splice(randomInd, 1);
+}
+
+// Now we need to randomize the array
+selected.sort(function() {
+    return 0.5 - Math.random();
+});
+
+// Create the tiles
+var tiles = [];
+for (var i = 0; i < NUM_COLS; i++) {
+    for (var j = 0; j < NUM_ROWS; j++) {
+        tiles.push(new Tile(i * 65 + 25, j * 65 + 25, selected.pop()));
+    }
+}
+
+background(255, 255, 255);
+
+// Now draw them face up
+for (var i = 0; i < tiles.length; i++) {
+    tiles[i].drawFaceDown();
+}
+
+var flippedTiles = [];
+var delayStartFC = null;
+var numTries = 0;
+var correctMatches = 0;
+var incorrectMatches = 0;
+
+mouseClicked = function() {
+    for (var i = 0; i < tiles.length; i++) {
+        if (tiles[i].isUnderMouse(mouseX, mouseY)) {
+            if (flippedTiles.length < 2 && !tiles[i].isFaceUp) {
+                tiles[i].drawFaceUp();
+                flippedTiles.push(tiles[i]);
+                if (flippedTiles.length === 2) {
+                    numTries++;
+                    if (flippedTiles[0].face === flippedTiles[1].face) {
+                        flippedTiles[0].isMatch = true;
+                        flippedTiles[1].isMatch = true;
+                        correctMatches++;
+                    }
+                    else {
+                        incorrectMatches++;
+                    }
+                    delayStartFC = frameCount;
+                    loop();
+                }
+            } 
+        }
+    }
+    var foundAllMatches = true;
+    for (var i = 0; i < tiles.length; i++) {
+        foundAllMatches = foundAllMatches && tiles[i].isMatch;
+    }
+    if (foundAllMatches) {
+        fill(0, 0, 0);
+        textSize(20);
+        text("You found them all in " + numTries + " tries!", 80, 360);
+        textSize(12);
+        text("You got " + correctMatches + " matches", 40, 380);
+        text("You guessed wrong " + incorrectMatches + " times", 210, 380);
+    }
+};
+
+draw = function() {
+    if (delayStartFC && (frameCount - delayStartFC) > 30) {
+        for (var i = 0; i < tiles.length; i++) {
+            if (!tiles[i].isMatch) {
+                tiles[i].drawFaceDown();
+            }
+        }
+        flippedTiles = [];
+        delayStartFC = null;
+        noLoop();
+    }
+};
